d83a032fffd73a0f28603175d4d6ea6c772c99cf
Lagomoro <2669708373@qq.com>
2024-05-29 02:25:06 +0800

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -69,7 +69,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/ComicTools/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
