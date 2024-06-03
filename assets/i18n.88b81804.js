ec82325a9d7479658b409c527397b3a1023a3c4e
Lagomoro <2669708373@qq.com>
2024-06-03 08:39:43 +0800

    Support Long Image

--- assets/i18n.88b81804.js
+++ assets/i18n.88b81804.js
@@ -1,4 +1,4 @@
-import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,b as Rn,r as kn,s as Dn,f as Mn,T as Fn,j as vn}from"./index.77890931.js";/*!
+import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,b as Rn,r as kn,s as Dn,f as Mn,T as Fn,j as vn}from"./index.af5373c7.js";/*!
   * shared v9.13.1
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
