dd51dbaec9285c2aa8014fc8e0d12b29c27959b0
Lagomoro <2669708373@qq.com>
2025-05-28 22:08:17 +0800

    Fix

--- src/scripts/util/ObjectUtil.ts
+++ src/scripts/util/ObjectUtil.ts
@@ -23,7 +23,7 @@ export class ObjectUtil{
           return;
         }
         set.add(value);
-      }else if (replacer) {
+      } else if (replacer) {
         const result = replacer(key, value);
         if(result !== undefined){
           return result;
@@ -39,6 +39,36 @@ export class ObjectUtil{
   // ------------------------------------------------------------------------------
   public static deepCopy<T>(object: T): T{
     return JSON.parse(this.toString(object));
+  };
+  // ------------------------------------------------------------------------------
+  //# endregion
+  // ------------------------------------------------------------------------------
+  //# region Map
+  // ------------------------------------------------------------------------------
+  public static map<K extends keyof any, T, V> (record: Record<K, T>, callbackFunction: (value: T, key: K, map: Record<K, T>) => V): Record<K, V> {
+    const output: Record<K, V> = {} as Record<K, V>;
+    for(const key in record){
+      output[key] = callbackFunction(record[key], key, record);
+    }
+    return output;
+  };
+
+  public static mapArray<K extends keyof any, T, V> (record: Record<K, T>, callbackFunction: (value: T, key: K, map: Record<K, T>) => V): V[] {
+    const output: V[] = [];
+    for(const key in record){
+      output.push(callbackFunction(record[key], key, record));
+    }
+    return output;
+  };
+
+  public static filter<K extends keyof any, T> (record: Record<K, T>, callbackFunction: (value: T, key: K, map: Record<K, T>) => boolean): Record<K, T> {
+    const output: Record<K, T> = {} as Record<K, T>;
+    for(const key in record){
+      if(callbackFunction(record[key], key, record)){
+        output[key] = record[key];
+      }
+    }
+    return output;
   };
   // ------------------------------------------------------------------------------
   //# endregion
